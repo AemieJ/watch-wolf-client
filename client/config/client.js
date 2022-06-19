f9df50d4f24d60137812e8b2c95c1b2fd6c5f1a6
AemieJ <aemie.j@hotmail.co.uk>
2022-06-19 15:13:27 +0530

    modify config for client

--- client/config/client.js
+++ client/config/client.js
@@ -1,6 +1,6 @@
-const dev = "development"
+const dev = "production"
 
 export const client =
   dev == "development"
     ? "http://localhost:3000"
-    : "https://microsoft-engage-21.vercel.app"
+    : "https://watch-wolf.vercel.app/"
