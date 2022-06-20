86722d55edb2b89fa60d1f4463fffcacb16b8260
AemieJ <aemie.j@hotmail.co.uk>
2022-06-20 11:33:25 +0530

    modify config

--- client/config/client.js
+++ client/config/client.js
@@ -1,6 +1,6 @@
-const dev = "development"
+const dev = "production"
 
 export const client =
   dev == "development"
     ? "http://localhost:3000"
-    : "https://watch-wolf.vercel.app/"
+    : "https://watch-wolf.vercel.app"
